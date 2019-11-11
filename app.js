c3e0416ea8147249a53a953b42261a0dd02d08c6
Mike Holzbach <mikeholzbach@carfax.com>
2019-11-11 11:58:43 -0500

    Types n stuff

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("It works!");
+// Variable complex is an object that has two keys, data and output. Data is an array on numbers, and output is a function that takes one argument "all" which can be named anything, but must have type boolean. The function returns an array of numbers.
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+complex = {};
--- app.ts
+++ app.ts
@@ -1,1 +1,26 @@
-console.log("It works!");
+// Variable complex is an object that has two keys, data and output. Data is an array on numbers, and output is a function that takes one argument "all" which can be named anything, but must have type boolean. The function returns an array of numbers.
+let complex: { data: number[]; output: (all: boolean) => number[] } = {
+  data: [100, 3.99, 10],
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+};
+
+//type alias
+type Complex = { data: number[]; output: (all: boolean) => number[] };
+
+let complex2: Complex = {
+  data: [1, 2, 3],
+  output: arg => {
+    return this.data;
+  }
+};
+
+//union types. Can string together as many types as you like.
+let myRealAge: number | string = 39;
+
+//check types at runtime
+let finalValue = "A string";
+if (typeof finalValue === "string") {
+  console.log(finalValue);
+}
