5f648857ca41d9471fdf0e0012ac5d59a39873fd
Mike Holzbach <mikeholzbach@carfax.com>
2019-11-21 13:47:23 -0500

    The basics

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var add = function (n1, n2) {
-    return n1 + n2;
+var add = function (n1, n2, printResult, phrase) {
+    return printResult ? console.log("" + phrase + (n1 + n2)) : n1 + n2;
 };
 var num1 = 2;
-var num2 = "derp";
-var result = add(num1, num2);
-console.log(result);
+var num2 = 3;
+var printResult = true;
+var resultPhrase = "The result is ";
+add(num1, num2, printResult, resultPhrase);
