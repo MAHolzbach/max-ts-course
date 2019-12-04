991ab40cd88e22c68ddddc3922d9c848cd39ddfc
Mike Holzbach <mikeholzbach@carfax.com>
2019-12-04 07:59:29 -0500

    Getters setters and abstracts

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ var Department = /** @class */ (function () {
         this.employees = [];
         // this.name = n;
     }
+    Department.createEmployee = function (name) {
+        return {
+            name: name
+        };
+    };
     Department.prototype.describe = function () {
         console.log("Department: " + this.name);
     };
@@ -27,6 +32,7 @@ var Department = /** @class */ (function () {
         console.log(this.employees.length);
         console.log(this.employees);
     };
+    Department.fiscalYear = 2020;
     return Department;
 }());
 var ITDepartment = /** @class */ (function (_super) {
@@ -38,11 +44,52 @@ var ITDepartment = /** @class */ (function (_super) {
     }
     return ITDepartment;
 }(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, "Accounting", id) || this;
+        _this.reports = reports;
+        _this.lastReport = reports[0];
+        return _this;
+    }
+    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
+        get: function () {
+            if (this.lastReport) {
+                return this.lastReport;
+            }
+            throw new Error("No report found.");
+        },
+        set: function (value) {
+            if (!value) {
+                throw new Error("Please provide report text.");
+            }
+            this.addReport(value);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    AccountingDepartment.prototype.addEmployee = function (name) {
+        if (name === "Mike") {
+            return;
+        }
+        this.employees.push(name);
+    };
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    };
+    return AccountingDepartment;
+}(Department));
+var employee1 = Department.createEmployee("Mark");
 var it = new ITDepartment(1, ["Mike"]);
-it.describe();
-it.addEmployee("Mike");
-it.addEmployee("Bert");
-it.printEmployeeInfo();
-it.addEmployee("Ernie");
-it.printEmployeeInfo();
-console.log(it);
+var accounting = new AccountingDepartment(2, []);
+console.log(Department.fiscalYear);
+// accounting.mostRecentReport = "Report number 5.";
+// console.log(accounting.mostRecentReport);
+// it.describe();
+// it.addEmployee("Mike");
+// it.addEmployee("Bert");
+// it.printEmployeeInfo();
+// it.addEmployee("Ernie");
+// it.printEmployeeInfo();
+// console.log(it);
