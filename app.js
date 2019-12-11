c186ef6f74ac413d8becb58d28ae82168a6df8c6
Mike Holzbach <mikeholzbach@carfax.com>
2019-12-11 08:03:07 -0500

    Setup

--- app.js
+++
@@ -1,8 +0,0 @@
-var add = function (n1, n2, printResult, phrase) {
-    return printResult ? console.log("" + phrase + (n1 + n2)) : n1 + n2;
-};
-var num1 = 2;
-var num2 = 3;
-var printResult = true;
-var resultPhrase = "The result is ";
-add(num1, num2, printResult, resultPhrase);
